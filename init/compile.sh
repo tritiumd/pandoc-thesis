063160b2fa19590613886df6c37687ca25de4d58
ngocptblaplafla <phanthanhngocblaplafla@gmail.com>
2024-01-07 23:17:44 +0700

    fix utf8 listings and twoside

--- init/compile.sh
+++ init/compile.sh
@@ -3,4 +3,4 @@ docker run --rm -v .:/workspace ngocptblaplafla/pandoc-texlive-full:latest \
       -L tables-rules.lua  -L column-div.lua -F pandoc-latex-environment  \
       -L include-code-files.lua -L multibib.lua  -L diagram.lua \
       --fail-if-warnings --from=markdown main.md --metadata-file=config.yaml \
-      --template=vnthesis-oneside.tex -s -o result.pdf
+      --template=vnthesis.tex -s -o result.pdf
