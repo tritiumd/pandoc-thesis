e41930485263214f006b393294e1bdd658edf198
ngocptblaplafla <phanthanhngocblaplafla@gmail.com>
2024-03-28 20:47:31 +0700

    fix typo for template update compile.sh for new version of pandoc-texlive-full

--- init/compile.sh
+++ init/compile.sh
@@ -1,5 +1,5 @@
-docker run --rm -v .:/workspace ngocptblaplafla/pandoc-texlive-full:latest --pdf-engine=xelatex\
-       --fail-if-warnings --listings --template=vnthesis.tex -L include-files.lua -L firstauthor.lua\
-      -L include-code-files.lua -L diagram.lua -L tables-rules.lua -L multibib.lua -L column-div.lua\
-      -F pandoc-latex-environment -s --verbose --from=markdown+emoji main.md --metadata-file=config.yaml\
-      --output result.pdf
+docker run --rm --user `id -u`:`id -g` -v .:/workspace ngocptblaplafla/pandoc-texlive-full:latest \
+      --pdf-engine=xelatex --fail-if-warnings --listings --template=vnthesis.tex -L include-files.lua \
+      -L firstauthor.lua -L include-code-files.lua -L diagram.lua -L tables-rules.lua -L multibib.lua \
+      -L column-div.lua -F pandoc-latex-environment -s --verbose -L fix-header.lua \
+      --from=markdown+emoji main.md --metadata-file=config.yaml --output result.pdf
